9bdf2e26ed4b8d3fb87e5cf6c44fc68146da2094
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2018-08-29 15:49:40 +0300

    Remove type checks in file system shim calls

--- ReSharper.FSharp/src/FSharp.ProjectModelBase/src/DelegatingFileSystemShim.cs
+++ ReSharper.FSharp/src/FSharp.ProjectModelBase/src/DelegatingFileSystemShim.cs
@@ -8,68 +8,73 @@ using static Microsoft.FSharp.Compiler.AbstractIL.Internal.Library;
 
 namespace JetBrains.ReSharper.Plugins.FSharp
 {
-    public abstract class FileSystemShimChangeProvider : DelegatingFileSystemShim
-    {
-        protected FileSystemShimChangeProvider(Lifetime lifetime, ChangeManager changeManager) : base(lifetime) =>
-            changeManager.Changed2.Advise(lifetime, Execute);
+  public abstract class FileSystemShimChangeProvider : DelegatingFileSystemShim
+  {
+    protected FileSystemShimChangeProvider(Lifetime lifetime, ChangeManager changeManager) : base(lifetime) =>
+      changeManager.Changed2.Advise(lifetime, Execute);
 
-        public virtual void Execute(ChangeEventArgs changeEventArgs)
-        {
-        }
+    public virtual void Execute(ChangeEventArgs changeEventArgs)
+    {
+    }
+  }
+
+  public class DelegatingFileSystemShim : Shim.IFileSystem
+  {
+    public DelegatingFileSystemShim(Lifetime lifetime)
+    {
+      OverridenFileSystem = Shim.FileSystem;
+      IsOverridingDelegating = OverridenFileSystem is DelegatingFileSystemShim;
+      Shim.FileSystem = this;
+      lifetime.AddAction(() => Shim.FileSystem = OverridenFileSystem);
     }
 
-    public class DelegatingFileSystemShim : Shim.IFileSystem
+    private Shim.IFileSystem OverridenFileSystem { get; }
+    private bool IsOverridingDelegating { get; }
+
+    private DelegatingFileSystemShim OverridenDelegatingFileSystem =>
+      (DelegatingFileSystemShim) OverridenFileSystem;
+
+    public virtual bool Exists(FileSystemPath path) =>
+      IsOverridingDelegating
+        ? OverridenDelegatingFileSystem.Exists(path)
+        : OverridenFileSystem.SafeExists(path.FullPath);
+
+    public virtual DateTime GetLastWriteTime(FileSystemPath path) =>
+      IsOverridingDelegating
+        ? OverridenDelegatingFileSystem.GetLastWriteTime(path)
+        : OverridenFileSystem.GetLastWriteTimeShim(path.FullPath);
+
+    [Obsolete("Use in tests only.")]
+    public virtual DateTime GetLastWriteTimeShim(string fileName)
     {
-        private readonly Shim.IFileSystem myFileSystem;
-
-        public DelegatingFileSystemShim(Lifetime lifetime)
-        {
-            myFileSystem = Shim.FileSystem;
-            Shim.FileSystem = this;
-            lifetime.OnTermination(() => Shim.FileSystem = myFileSystem);
-        }
-
-        public virtual bool Exists(FileSystemPath path) =>
-            myFileSystem is DelegatingFileSystemShim shim
-                ? shim.Exists(path)
-                : myFileSystem.SafeExists(path.FullPath);
-
-        public virtual DateTime GetLastWriteTime(FileSystemPath path) =>
-            myFileSystem is DelegatingFileSystemShim shim
-                ? shim.GetLastWriteTime(path)
-                : myFileSystem.GetLastWriteTimeShim(path.FullPath);
-
-        [Obsolete("Use in tests only.")]
-        public virtual DateTime GetLastWriteTimeShim(string fileName)
-        {
-            var path = FileSystemPath.TryParse(fileName);
-            return path.IsEmpty
-                ? myFileSystem.GetLastWriteTimeShim(fileName)
-                : GetLastWriteTime(path);
-        }
-
-        [Obsolete("Use in tests only.")]
-        public virtual bool SafeExists(string fileName)
-        {
-            var path = FileSystemPath.TryParse(fileName);
-            return path.IsEmpty
-                ? myFileSystem.SafeExists(fileName)
-                : Exists(path);
-        }
-
-        public virtual byte[] ReadAllBytesShim(string fileName) => myFileSystem.ReadAllBytesShim(fileName);
-        public virtual Stream FileStreamReadShim(string fileName) => myFileSystem.FileStreamReadShim(fileName);
-        public virtual Stream FileStreamCreateShim(string fileName) => myFileSystem.FileStreamCreateShim(fileName);
-        public virtual string GetFullPathShim(string fileName) => myFileSystem.GetFullPathShim(fileName);
-        public virtual bool IsPathRootedShim(string path) => myFileSystem.IsPathRootedShim(path);
-        public virtual bool IsInvalidPathShim(string filename) => myFileSystem.IsInvalidPathShim(filename);
-        public virtual string GetTempPathShim() => myFileSystem.GetTempPathShim();
-        public virtual void FileDelete(string fileName) => myFileSystem.FileDelete(fileName);
-        public virtual Assembly AssemblyLoadFrom(string fileName) => myFileSystem.AssemblyLoadFrom(fileName);
-        public virtual Assembly AssemblyLoad(AssemblyName assemblyName) => myFileSystem.AssemblyLoad(assemblyName);
-        public virtual bool IsStableFileHeuristic(string fileName) => myFileSystem.IsStableFileHeuristic(fileName);
-
-        public virtual Stream FileStreamWriteExistingShim(string fileName) =>
-            myFileSystem.FileStreamWriteExistingShim(fileName);
+      var path = FileSystemPath.TryParse(fileName);
+      return path.IsEmpty
+        ? OverridenFileSystem.GetLastWriteTimeShim(fileName)
+        : GetLastWriteTime(path);
     }
+
+    [Obsolete("Use in tests only.")]
+    public virtual bool SafeExists(string fileName)
+    {
+      var path = FileSystemPath.TryParse(fileName);
+      return path.IsEmpty
+        ? OverridenFileSystem.SafeExists(fileName)
+        : Exists(path);
+    }
+
+    public virtual byte[] ReadAllBytesShim(string fileName) => OverridenFileSystem.ReadAllBytesShim(fileName);
+    public virtual Stream FileStreamReadShim(string fileName) => OverridenFileSystem.FileStreamReadShim(fileName);
+    public virtual Stream FileStreamCreateShim(string fileName) => OverridenFileSystem.FileStreamCreateShim(fileName);
+    public virtual string GetFullPathShim(string fileName) => OverridenFileSystem.GetFullPathShim(fileName);
+    public virtual bool IsPathRootedShim(string path) => OverridenFileSystem.IsPathRootedShim(path);
+    public virtual bool IsInvalidPathShim(string filename) => OverridenFileSystem.IsInvalidPathShim(filename);
+    public virtual string GetTempPathShim() => OverridenFileSystem.GetTempPathShim();
+    public virtual void FileDelete(string fileName) => OverridenFileSystem.FileDelete(fileName);
+    public virtual Assembly AssemblyLoadFrom(string fileName) => OverridenFileSystem.AssemblyLoadFrom(fileName);
+    public virtual Assembly AssemblyLoad(AssemblyName assemblyName) => OverridenFileSystem.AssemblyLoad(assemblyName);
+    public virtual bool IsStableFileHeuristic(string fileName) => OverridenFileSystem.IsStableFileHeuristic(fileName);
+
+    public virtual Stream FileStreamWriteExistingShim(string fileName) =>
+      OverridenFileSystem.FileStreamWriteExistingShim(fileName);
+  }
 }
