096242e8107ac1188cc88e58044137584b8ad984
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2019-08-04 23:04:43 +0300

    Add getting F# signature metadata resources

--- ReSharper.FSharp/src/FSharp.Common/src/Util/FSharpAssemblyUtil.fs
+++ ReSharper.FSharp/src/FSharp.Common/src/Util/FSharpAssemblyUtil.fs
@@ -1,6 +1,8 @@
 [<AutoOpen; Extension>]
 module JetBrains.ReSharper.Plugins.FSharp.Util.FSharpAssemblyUtil
 
+open JetBrains.Diagnostics
+open JetBrains.Metadata.Reader.API
 open JetBrains.ProjectModel
 open JetBrains.ReSharper.Psi
 open JetBrains.ReSharper.Psi.Modules
@@ -28,3 +30,42 @@ let isFSharpAssembly (psiModule: IPsiModule) =
         isFSharpAssembly
 
     | value -> value == BooleanBoxes.True
+
+let [<Literal>] signatureInfoResourceName = "FSharpSignatureInfo."
+let [<Literal>] signatureInfoResourceNameOld = "FSharpSignatureData."
+
+let isSignatureDataResource (manifestResource: IMetadataManifestResource) (compilationUnitName: outref<string>) =
+    let name = manifestResource.Name
+    if startsWith signatureInfoResourceName name then
+        compilationUnitName <- name.Substring(signatureInfoResourceName.Length)
+        true
+    elif startsWith signatureInfoResourceNameOld name then
+        compilationUnitName <- name.Substring(signatureInfoResourceNameOld.Length)
+        true
+    else false
+
+let getFSharpSignatureInfos (psiModule: IPsiModule) =
+    match psiModule.As<IAssemblyPsiModule>() with
+    | null -> null
+    | assemblyPsiModule ->
+
+    let path = assemblyPsiModule.Assembly.Location
+    if isNull path then null else
+
+    Assertion.Assert(isFSharpAssembly psiModule, "isFSharpAssembly psiModule")
+
+    let metadataLoader = new MetadataLoader()
+    let metadataAssembly = metadataLoader.TryLoadFrom(path, JetFunc<_>.False)
+    if isNull metadataAssembly then null else
+
+    let resources = metadataAssembly.GetManifestResources()
+
+    for manifestResource in resources do
+        let mutable compilationUnitName = Unchecked.defaultof<_>
+        if not (isSignatureDataResource manifestResource &compilationUnitName) then () else
+
+        use stream = manifestResource.GetDisposition().CreateResourceReader()
+        // todo: add metadata reading
+        ()
+
+    null
